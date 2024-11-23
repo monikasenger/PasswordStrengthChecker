64450a9ce4f0b0622c7e1552f58663f2af16f11d
Monika Senger <107236089+monikasenger@users.noreply.github.com>
2024-11-23 20:38:46 +0530

    Password Strength Checker
    Password Strength Checker

---
+++ PasswordStrengthChecker/src/PasswordStrengthChecker.java
@@ -0,0 +1,11 @@
+/**
+ * main class to start the password strength checker
+ */
+
+public class PasswordStrengthChecker {
+    // main method to run the PasswordStrength program
+    public static void main(String[] args) {
+        PasswordStrength pass = new PasswordStrength(); //  PasswordStrength class instance
+        pass.passwordStrength(); // call password strength checker method
+    }
+}
